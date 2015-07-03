ab68f1b707907e44c7a854faf22e6dfe842f3c29
Mathias Nakatsui <mnakatsui@eastcor.com>
2015-07-02 17:00:57 -0700

    fixed styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -257,7 +257,7 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.dist %>/images/{,*/}*.{gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
